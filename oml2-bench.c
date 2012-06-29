d71b8cfd018f8b525a8be16c33899b27dab6ebe0
Nathan Samson <nathansamson@gmail.com>
2012-06-29 20:25:51 +0200

    First working prototype

--- oml2-bench.c
+++ oml2-bench.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 #include <oml2/omlc.h>
 
 int main(int argc, const char** argv) {
-	int result = omlc_init("OML2 Bench", &argc, argv, 0);
-	int count = 10000;
+	int result = omlc_init("OML2Bench", &argc, argv, 0);
+	uint64_t max_count = atoi(argv[2]);
+	uint64_t current_count = 0;
+	useconds_t delay = 1.0 / atoi(argv[1]) * 1000 * 1000;
 	if (result == -1) {
 		fprintf(stderr, "Could not initialize OML\n");
 		exit(1);	
@@ -12,27 +15,29 @@ int main(int argc, const char** argv) {
 
 	OmlMPDef mp_def[] = {
 		{ "text", OML_STRING_VALUE },
-		{ "counter", OML_INT_VALUE }
+		{ "counter", OML_UINT64_VALUE },
 		{ 0, (OmlValueT)0 },
 	};
-	OmlMP* mp = omld_add_mp("log_info", mp_def);
+	OmlMP *mp = omlc_add_mp("log_info", mp_def);
 
 	if (omlc_start() == -1) {
-		fprintf(stderr; "Error starting up OML streams\n");
+		fprintf(stderr, "Error starting up OML streams\n");
 		exit(1);
 	}
 
 	OmlValueU values[2];
 
-	for (int counter = 0; counter < count; count++) {
-		char text[256];
+	for (current_count = 1; current_count <= max_count; current_count++) {
+		char text[512];
 		
 		sprintf(text, "This is some string that I will be logging " \
-                              "as fast as I can. This is the %i that I sent.", 
-                        counter);
-
-		oml_set_string(values[0], text);
-		oml_set_counter(values[1], count);
+                       "as fast as I can. This is the %u that I sent.\n", 
+                current_count);
+		printf(text);
+		omlc_set_string(values[0], text);
+		omlc_set_uint64(values[1], current_count);
+		omlc_inject(mp, values);
+		usleep(delay);
 	}
 
 	omlc_close();
